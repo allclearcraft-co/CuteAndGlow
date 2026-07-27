cf643f87816915c37f8be8fa0972ef7010b11415
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-27 18:51:58 +0530

    feat: enhance createStoreService to handle image uploads and sanitize input data

--- backend/src/controllers/service.controller.js
+++ backend/src/controllers/service.controller.js
@@ -36,6 +36,39 @@ const createStoreService = asyncHandler(async (req, res) => {
   const store = await Store.findById(storeId);
   if (!store) throw new ApiError(400, "Invalid access !");
 
+  const sanitize = (str = "") =>
+    str
+      .toString()
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9-_]/g, "")
+      .replace(/\s+/g, "-");
+  const safeName = sanitize(store.name);
+
+  const images = req.files || [];
+  const uploadedImages = [];
+
+  for (const img of images) {
+    const uploaded = await UploadImages(img.filename, {
+      folderStructure: `store/${safeName
+        .trim()
+        .replace(/\s+/g, "-")
+        .toLowerCase()}/serviceCoverImage`,
+    });
+
+    uploadedImages.push({
+      url: uploaded.url,
+      fileId: uploaded.fileId,
+      altText: name,
+    });
+  }
+
+  const sanitizePrepTime = isPrepTime === "on" ? true : false;
+  const sanitizeTimeIncludingPrepTime =
+    timeIncludingPrepTime === "on" ? true : false;
+  const sanitizeOnSite = onSite === "on" ? true : false;
+  const sanitizeInHouse = inHouse === "on" ? true : false;
+
   const service = await Services.create({
     name,
     store: storeId,
@@ -46,18 +79,24 @@ const createStoreService = asyncHandler(async (req, res) => {
     serviceExclusion: serviceExclusion,
     duration,
     prepTime,
-    isPrepTime,
-    timeIncludingPrepTime,
-    onSite,
-    inHouse,
+    isPrepTime: sanitizePrepTime,
+    timeIncludingPrepTime: sanitizeTimeIncludingPrepTime,
+    onSite: sanitizeOnSite,
+    inHouse: sanitizeInHouse,
     serviceFor,
     charges,
     bookingDays,
-    bookingAcceptingHours,
-    // image
+    bookingAcceptingHours: { from: bookingFrom, till: bookingTill },
+    coverImage: uploadedImages,
     serviceArea,
     serviceRequirements,
   });
+  if (!service)
+    throw new ApiError(403, "Something went wrong please try again later");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, service, "Added successfully !"));
 });
 
 export { createStoreService };
