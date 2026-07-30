6b509bc79f36405c995812c111d68aa9ce66621f
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-07-30 11:56:07 +0530

    feat: refactor Home component to improve service fetching and display with loading and empty states

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Landing from "./Landing";
 import ServiceCard from "./ServiceCard";
 import ChooseUs from "./ChooseUs";
-import ServiceGrid from "../Services/ServiceGrid";
+import ServiceGrid from "../Services/components/ServiceGrid";
 import { services } from "../../constants/service";
 import Button from "../../components/Button";
 import { useNavigate } from "react-router-dom";
@@ -10,28 +10,48 @@ import BookingServiceCard from "../../components/ui/StoreServiceCard";
 import { FetchData } from "../../utils/FetchFromApi";
 import { useEffect } from "react";
 import { useState } from "react";
+import EmptyState from "../Services/components/EmptyState";
+import LoadingSkeleton from "../Services/components/LoadingSkeleton";
 
 function Home() {
-  const demoService = services.slice(2, services.length);
   const navigate = useNavigate();
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    page: 1,
+    limit: 9,
+    category: "",
+    serviceFor: "",
+    search: "",
+    sortBy: "latest",
+  });
 
   const getBookingServices = async () => {
     try {
+      setLoading(true);
+      const params = new URLSearchParams();
+      Object.entries(filters).forEach(([key, value]) => {
+        if (value !== "" && value !== undefined && value !== null) {
+          params.append(key, value);
+        }
+      });
       const response = await FetchData(
-        `services/get/service/all-service/${"heroSection"}/${6}`,
+        `services/get/service?${params.toString()}`,
         "get",
       );
       console.log(response);
-      setData(response.data.data);
+      setData(response.data.data.services);
+      // setPagination(response.data.data.pagination);
     } catch (err) {
-      console.log(err.response.data);
+      console.log(err.response?.data || err);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getBookingServices();
-  }, []);
+  }, [filters]);
 
   return (
     <div className="h-full pb-10 flex flex-col gap-10 relative">
@@ -43,7 +63,16 @@ function Home() {
         <h1 className="font-medium text-3xl w-full text-center heading">
           Explore Our Services
         </h1>
-        <ServiceGrid services={data} cssDisplay="hero" />
+        {loading ? (
+          <LoadingSkeleton />
+        ) : data.length === 0 ? (
+          <EmptyState />
+        ) : (
+          <>
+            <ServiceGrid services={data} />
+          </>
+        )}
+        {/* <ServiceGrid services={data} /> */}
         <Button
           LabelName="Explore More"
           onClick={() => navigate("/services/all")}
