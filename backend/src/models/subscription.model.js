1d64ebb67d882598ed6f789aa2879660f809903b
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-08-10 13:35:22 +0530

    feat: implement subscription model and controller with CRUD operations

--- backend/src/models/subscription.model.js
+++ backend/src/models/subscription.model.js
@@ -1,23 +1,33 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
-  customer: { type: mongoose.Schema.Types.ObjectId, ref: "Customer" },
-  professional: { type: mongoose.Schema.Types.ObjectId, ref: "Professional" },
-  store: { type: mongoose.Schema.Types.ObjectId, ref: "Store" },
-
-  planFor: {
-    type: String,
-    enum: ["Customer", "Store", "Professional"],
-    default: "Customer",
+const subscriptionSchema = new mongoose.Schema(
+  {
+    admin: { type: mongoose.Schema.Types.ObjectId, ref: "Admin" },
+    planName: {
+      type: String,
+      enum: ["platinum", "gold", "silver", "bronze", "basic"],
+      default: "basic",
+    },
+    planFor: {
+      type: String,
+      enum: ["customer", "store", "professional", "custom"],
+      default: "store",
+    },
+    customModelFor: String,
+    customModel: { type: Boolean, default: false },
+    tagline: String,
+    price: {
+      mrp: { type: Number, required: true },
+      discount: Number,
+      sellingPrice: Number,
+    },
+    features: [String],
+    planPrice: String,
+    validity: Number,
+    faqs: [{ question: String, answer: String }],
+    isActive: { type: Boolean, default: true },
   },
-
-  plan: {
-    type: String,
-    enum: ["Free", "Third", "Second", "First"],
-    default: "Free",
-  },
-  planPrice: String,
-  validity: String,
-});
+  { timestamps: true },
+);
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
