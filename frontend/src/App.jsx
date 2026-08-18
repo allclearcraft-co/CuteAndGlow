0857096528675bb79f94e386bac55ad4c0b4701f
KSHITIJ SAXENA <133942471+kssaxena@users.noreply.github.com>
2026-08-18 17:54:11 +0530

    feat: add admin authentication routes and update re-login endpoint mapping

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,6 +17,7 @@ import ServiceBooking from "./pages/ServiceBooking/ServiceBooking";
 import AdminDashboard from "./pages/Admin/AdminDashboard";
 import TermsAndConditions from "./pages/CMS/TermsAndConditions";
 import Policy from "./pages/CMS/Policy";
+import AdminAuth from "./pages/Auth/AdminAuth";
 
 function App() {
   const location = useLocation();
@@ -40,11 +41,14 @@ function App() {
         if (!role || !refreshToken) {
           throw new Error("Missing auth data");
         }
-
         const endpointMap = {
           Customer: "customer/auth/re-login",
           Store: "store/auth/re-login",
           Professional: "professional/auth/re-login",
+          admin: "admin/auth/re-login",
+          subAdmin: "admin/auth/re-login",
+          sales: "admin/auth/re-login",
+          marketing: "admin/auth/re-login",
         };
         const endpoint = endpointMap[role];
         if (!endpoint) {
@@ -98,6 +102,11 @@ function App() {
 
           {/* =========================ADMIN ================================== */}
           {/* <Route path="/admin/authentication/login" element={<AdminAuth />} />  */}
+          <Route path="/admin/login" element={<AdminAuth />} />
+          <Route
+            path="/admin/reset/password"
+            element={<AdminAuth resetPassword={true} />}
+          />
           <Route path="/admin/dashboard" element={<AdminDashboard />} />
         </Routes>
       </div>
